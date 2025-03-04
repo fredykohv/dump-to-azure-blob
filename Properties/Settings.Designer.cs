9e40a26432ffa537c9932414cf59217b16d74c1e
fredykohv <fredykohv14@gmail.com>
2025-03-04 09:50:14 +0200

    Properties: Add Setting to set/get ConnectionString and Container Name

---
+++ Properties/Settings.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace DumpToAzureBlob.Properties 
+{
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase 
+    {
+        
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        
+        public static Settings Default 
+        {
+            get 
+            {
+                return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string ConnectionString 
+        {
+            get 
+            {
+                return ((string)(this["ConnectionString"]));
+            }
+            set 
+            {
+                this["ConnectionString"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string ContainerName 
+        {
+            get 
+            {
+                return ((string)(this["ContainerName"]));
+            }
+            set 
+            {
+                this["ContainerName"] = value;
+            }
+        }
+    }
+}
