196378e29b164633e8dfea4b7750759ad4045d56
fredykohv <fredykohv14@gmail.com>
2025-03-07 01:21:18 +0200

    QoL: UI
    - Windows 11 like colours
    - Settings is now a Tab, along with Upload and Download

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -475,6 +475,21 @@ namespace DumpToAzureBlob
             }
         }
 
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
         protected override void OnClosing(CancelEventArgs e)
         {
             foreach (var watcher in _folderWatchers.Values)
